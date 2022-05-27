a5887ff20dbe92a74b096514fc67992681f7a5d3
lommel <monkeytitle@hotmail.com>
2022-05-27 23:16:59 +0700

    add:react-icon"

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import { supabase } from "../login/supabaseClient";
+import { Stack, Grid,Button } from "@mui/material";
 function Login() {
+  async function signInWithGithub() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "github",
+    });
+  }
   return (
-    <div>login</div>
-  )
+    <div>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ backgroundColor: "black", width: "100%", height: "100%" }}
+      >
+        <Grid item xs={12} lg={4} sx={{ backgroundColor: "pink" }}>
+          <Stack spacing={2} direction="column">
+            <h1>Log in</h1>
+            <Button onClick={signInWithGithub}>
+              
+              Sign in with Google
+              </Button>
+            <Button onClick={signInWithGithub}>Sign in with Github</Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
