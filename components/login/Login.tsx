7def0d4d90976be4fd42d2580f9847fb5101b915
lommel <monkeytitle@hotmail.com>
2022-05-28 17:24:43 +0700

    add:curve-success

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -3,7 +3,10 @@ import { useState } from "react";
 import { supabase } from "../login/supabaseClient";
 import { Stack, Grid, Button, Box } from "@mui/material";
 import { FcGoogle } from "react-icons/fc";
-import {GoogleLoginButton,GithubLoginButton} from "react-social-login-buttons";
+import {
+  GoogleLoginButton,
+  GithubLoginButton,
+} from "react-social-login-buttons";
 import Image from "next/image";
 
 function Login() {
@@ -20,17 +23,47 @@ function Login() {
         alignItems="center"
         sx={{ backgroundColor: "white", height: "100vh" }}
       >
-
-        <Grid item xs={12} md={6} lg={4} sx={{ backgroundColor: "pink",borderRadius:5,borderStyle:"solid" }}>
-          <Stack spacing={2} direction="column"  alignItems="center">
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
+          sx={{
+            backgroundColor: "pink",
+            borderRadius: "5px 5px 0px 0px",
+            borderStyle: "solid",
+          }}
+        >
+          <Stack spacing={2} direction="column" alignItems="center">
             <h1>Log in</h1>
-            <GoogleLoginButton onClick={() => alert("Hello")} style={{width:"45%",fontFamily:"Courier Prime",fontweight:"bolder"}}/>
-            <GithubLoginButton onClick={() => alert("Hello")} style={{width:"45%",marginTop:15,fontFamily:"Courier Prime"}}/>
-            <Box sx={{paddingTop:5}}>
-            <Image src="/svg/icon/icon-lock.svg" width={50} height={50} />
+            <GoogleLoginButton
+              onClick={() => alert("Hello")}
+              style={{
+                width: "45%",
+                fontFamily: "Courier Prime",
+                fontweight: "bolder",
+              }}
+            />
+            <GithubLoginButton
+              onClick={() => alert("Hello")}
+              style={{
+                width: "45%",
+                marginTop: 15,
+                fontFamily: "Courier Prime",
+              }}
+            />
+            <Box sx={{ paddingTop: 5 }}>
+              <Image src="/svg/icon/icon-lock.svg" width={50} height={50} />
             </Box>
           </Stack>
+          <div style={{paddingBottom:0,position:"relative"}}>
+          <img src="/svg/icon/wave2.svg" style={{objectFit:"cover",paddingBottom:0,marginBottom:-6}} />
+
+          </div>
+          
         </Grid>
+
+        
       </Grid>
     </div>
   );
