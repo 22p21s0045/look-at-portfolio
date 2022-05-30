6bcd8fa5c638a9f3fb59e5ecb485998acd348d2d
lommel <monkeytitle@hotmail.com>
2022-05-30 13:56:48 +0700

    add:tip-avatar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { AppBar, Button, Toolbar, Box, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Box,
+  Typography,
+  Avatar,
+  IconButton,
+  Tooltip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  DialogContentText,
+  FormControl,
+  Select,
+} from "@mui/material";
 import Image from "next/image";
-
+import { FiMenu } from "react-icons/fi";
 function Navbar() {
   return (
     <div>
@@ -11,11 +27,10 @@ function Navbar() {
       >
         <Toolbar sx={{ marginTop: 1 }}>
           <Button
-          className = "Button-add"
+            className="Button-add"
             sx={{
               backgroundColor: "#F4BBBB",
               fontfamily: "Courier Prime",
-              
             }}
           >
             <Image src="/svg/icon/navbar/add.svg" width="40" height="40" />
@@ -25,13 +40,43 @@ function Navbar() {
                 color: "black",
               }}
             >
-              <Typography sx={{ fontFamily: "Courier Prime",fontWeight:"bold"}}>
+              <Typography
+                sx={{ fontFamily: "Courier Prime", fontWeight: "bold" }}
+              >
                 add new
               </Typography>
             </Box>
           </Button>
+          <Tooltip title="Account setting">
+            <Avatar sx={{ position: "absolute", right: "20%" }} />
+          </Tooltip>
+
+          <IconButton sx={{ position: "absolute", right: "5%" }}>
+            <FiMenu color="white" size={40} />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Dialog
+        open={false}
+        onClose={() => {}}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Add new history"}</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <Select label="coin"></Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => {}} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={() => {}} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
