29dece9c4034dff7cc39d43fbf4453640a0dccab
lommel <monkeytitle@hotmail.com>
2022-06-05 13:33:24 +0700

    add: set up graph

---
+++ components/graph/Graph_routine.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper } from "@mui/material";
+import { Line } from 'react-chartjs-2';
+function Graph_routine() {
+  return (
+    <div>
+      <Paper></Paper>
+    </div>
+  );
+}
+
+export default Graph_routine;
--- pages/dashboard/[id].tsx
+++ pages/dashboard/[id].tsx
@@ -2,11 +2,17 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import type { Props } from "../../components/navbar/Navbar";
 import {InferGetStaticPropsType} from "next";
+import { Grid } from '@mui/material';
+import Graph_routine from "../../components/graph/Graph_routine";
 function id({coin}:InferGetStaticPropsType<typeof getServerSideProps>) {
   return (
     <div>
       <Navbar coin={coin} />
-      [id]
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Graph_routine/>
+          </Grid>
+        </Grid>
     </div>
   );
 }
