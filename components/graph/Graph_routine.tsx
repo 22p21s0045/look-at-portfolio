37778b6b46fc03b30501d6d64f40010d85657018
lommel <monkeytitle@hotmail.com>
2022-06-05 20:08:26 +0700

    fix: date format

--- components/graph/Graph_routine.tsx
+++ components/graph/Graph_routine.tsx
@@ -1,23 +1,62 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Paper } from "@mui/material";
 import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import { supabase } from "../../components/login/supabaseClient";
-import type {DATA} from "./type"
-function Graph_routine() {
-  const [data, setdata] = useState <Array<DATA>|null>(null);
-  const getdata = async () => {
-    const { data, error } = await supabase.from<DATA>("Historys").select("buy,created_at");
-    setdata(data);
-  };
-useEffect(()=>{
-  getdata();
-  console.log(data);
-}
-,[]);
+import type { DATA } from "./type";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+function Graph_routine(data: any) {
+  const [graph_data, setGraph_data] = useState(data);
+  useEffect(() => {
+    setGraph_data(graph_data);
+    console.log(graph_data);
+  }, [graph_data]);
   return (
     <div>
-      <Paper></Paper>
+      <Paper>
+        <Line
+          data={{
+            labels: graph_data.data.body.map((item: any) => new Date (item.created_at).toLocaleDateString()),
+            datasets: [
+              {
+                label: "Buy",
+
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "#000",
+
+                pointHitRadius: 10,
+
+                data: graph_data.data.body.map((item: any) => item.buy),
+              },
+            ],
+          }}
+          options={{
+            elements: {
+              line: {
+                tension: 0, // disables bezier curves
+              },
+            },
+          }}
+        />
+      </Paper>
     </div>
   );
 }
--- pages/dashboard/[id].tsx
+++ pages/dashboard/[id].tsx
@@ -2,16 +2,18 @@ import React from "react";
 import {useState, useEffect} from "react";
 import Navbar from "../../components/navbar/Navbar";
 import type { Props } from "../../components/navbar/Navbar";
+import type {DATA} from "../../components/graph/type";
 import {InferGetStaticPropsType} from "next";
 import { Grid } from '@mui/material';
+import {supabase} from "../../components/login/supabaseClient";
 import Graph_routine from "../../components/graph/Graph_routine";
-function id({coin}:InferGetStaticPropsType<typeof getServerSideProps>) {
+function id({coin,graph_data}:InferGetStaticPropsType<typeof getServerSideProps>) {
   return (
     <div>
       <Navbar coin={coin} />
       <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Graph_routine/>
+        <Grid item xs={12} md ={6} lg={6} sx={{marginTop:"10vh"}}>
+          <Graph_routine data={graph_data}/>
           </Grid>
         </Grid>
     </div>
@@ -20,10 +22,12 @@ function id({coin}:InferGetStaticPropsType<typeof getServerSideProps>) {
 export async function getServerSideProps() {
   const res = await fetch("https://api.bitkub.com/api/market/symbols");
   const data:Props = await res.json();
+  const graph_data= await supabase.from<DATA>("Historys").select("buy,created_at");
 
   return {
     props: {
       coin: data,
+      graph_data:graph_data
     },
   };
 }
