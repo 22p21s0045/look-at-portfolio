b18ce85717eebdd524d204bff18a28ee985c3eb0
lommel <monkeytitle@hotmail.com>
2022-06-10 16:19:36 +0700

    add: get sum profit supabase success

--- components/graph/Graph_profit.tsx
+++ components/graph/Graph_profit.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Paper } from '@mui/material';
+import axios from 'axios';
+import {supabase} from '../login/supabaseClient'
+const supabase_sum = async () =>{
+  const { data, error } = await supabase.rpc('get_sum_buy')
+  console.log(data)
+  return data
+}
 function Graph_profit(data: any) {
+  supabase_sum()
   return (
     <div>Graph_profit</div>
   )
