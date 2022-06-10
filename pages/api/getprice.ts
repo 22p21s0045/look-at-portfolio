ea35758a81a8d96a386d66e5d0bd2720ad57220c
lommel <monkeytitle@hotmail.com>
2022-06-10 13:08:23 +0700

    add: sum latest price success  😃

--- pages/api/getprice.ts
+++ pages/api/getprice.ts
@@ -1,41 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {supabase} from "../../components/login/supabaseClient";
-const getdata = async () =>{
-    const {data,error} = await supabase.from("Historys").select("coin_pair,amount")
-    console.log(data)
-    return data
-}
-const lastest_price = async() =>{
-    const data = await fetch("https://api.bitkub.com/api/market/ticker")
-    const json = await data.json()
-    return json
-}
+import { createNextState } from "@reduxjs/toolkit";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../components/login/supabaseClient";
+const getdata = async () => {
+  const { data, error } = await supabase
+    .from("Historys")
+    .select("coin_pair,amount");
+
+  return data;
+};
+const lastest_price = async () => {
+  const data = await fetch("https://api.bitkub.com/api/market/ticker");
+  const json = await data.json();
+  return json;
+};
 type Data = {
-  name: string
-  sum:number
+  
+  sum: number;
+};
+
+type Client_type = {
+  coin_pair: string;
+  amount: number;
 }
-
-export default function handler(
-
+type group_type = {
+  [key: string]: Client_type|null;
+}
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    const amount:any = getdata()
-    const lastest_price_data:any = lastest_price()
-    const sum = 0
-    amount.forEach(element => {
-        lastest_price_data.forEach(element2 => {
-            if(element.coin_pair == element2[element.coin_pair]){
-                sum += element.amount * element2.last
-            }
-        });
-    });
-   
- 
+  const amount:any = await getdata();
+  const lastest_price_data: any = await lastest_price();
+
+  let sum = 0;
+  amount.forEach((element:Client_type) => {
     
+    if ( Object.keys(lastest_price_data).includes(element.coin_pair)) {
+      sum += element.amount * lastest_price_data[element.coin_pair].last;
+    }
+  });
 
-
-
-  res.status(200).json({ name: 'John Doe', sum: sum });
+  res.status(200).json({  sum: sum });
 }
